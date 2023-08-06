bd5ca9e82026559d5212a5d81ef0acfd5b02af0a
ashutoshPanda <iamashutoshpanda@gmail.com>
2023-08-06 23:56:22 +0530

    Added logs

--- src/validation-schemas/cronJobs.js
+++ src/validation-schemas/cronJobs.js
@@ -1,23 +1,6 @@
 import * as yup from "yup";
 import cronParser from "cron-parser";
 
-const commonSchema = {
-  code: yup.string().required(),
-  timeoutMilliSeconds: yup.number().positive().integer().required(),
-  cron: yup
-    .string()
-    .required()
-    .test("valid-cron", "Invalid cron string", (value) => {
-      try {
-        cronParser.parseExpression(value);
-        return true;
-      } catch (error) {
-        return false;
-      }
-    }),
-  endTime: yup.date().required().min(new Date(), "End time should be greater than current time"),
-};
-
 export const cronJobSchemaCreate = yup.object().shape({
   code: yup.string().required(),
   timeoutMilliSeconds: yup.number().positive().integer().required(),
